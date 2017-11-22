639373aa81d759b2b0b64f1d8ba15fa209f67df5
Tommy Danielsson <tdserviceco@gmail.com>
2017-11-22 18:59:11 +0100

    took away title since it will be generated from functions.php

--- parts/part-head.php
+++ parts/part-head.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="<?php // ?>" />
-    <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
     <?php wp_head() ?>
 </head>
 <body <?php body_class() ?>>
